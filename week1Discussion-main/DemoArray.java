6fa4a87343c9c562f74666c54aa2cd388961903d
Brian Gasca <bgasca@ucsd.edu>
2024-01-10 15:14:29 -0800

    Delete week1Discussion-main directory

--- week1Discussion-main/DemoArray.java
+++
@@ -1,9 +0,0 @@
-public interface DemoArray<E extends Comparable<E>>{
-    
-    /* Get the smallest element in the array*/
-    public E min();
-  
-    /* Get the ith element in the array*/
-    public E get(int i);
-
-}
